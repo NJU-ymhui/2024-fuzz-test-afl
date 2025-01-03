334bb185521e385b87a9e45a76429bee7de25941
yuminghui <221870120@smail.nju.edu.cn>
2025-01-03 17:42:00 +0800

    formalize code style

--- src/main/java/cn/edu/nju/util/manager/FuzzingManager.java
+++ src/main/java/cn/edu/nju/util/manager/FuzzingManager.java
@@ -63,40 +63,39 @@ public class FuzzingManager {
      * @param objPath 源代码构建出来的可执行文件路径
      */
     public void runOn(String objPath) {
-        if (mutation == null || seedsManager == null || scheduler == null||resourcesManager == null||monitor == null||executor == null||evaluator==null) {
+        if (mutation == null || seedsManager == null || scheduler == null || resourcesManager == null || monitor == null || executor == null || evaluator==null) {
             Log.error("Some modules are not registered");
             throw new RuntimeException("Some modules are not registered");
         }
         int loops = loopCount;
 
-            while (loops-- > 0) {
-                monitor.setUp(); // 开始监控
-                seedsManager.register(this.resourcesManager);
-                scheduler.register(this.resourcesManager);
+        while (loops-- > 0) {
+            monitor.setUp(); // 开始监控
+            seedsManager.register(this.resourcesManager);
+            scheduler.register(this.resourcesManager);
 
-                seedsManager.sort(); // 种子排序
-                scheduler.schedule(); // 能量调度
+            seedsManager.sort(); // 种子排序
+            scheduler.schedule(); // 能量调度
 
-                // 获取下一个种子
-                SeedsManagerImpl.Seed seed = seedsManager.getNextSeed();
-                if (seed == null) {
-                    Log.error("No seeds available to mutate.");
-                    break;
-                }
-
-                // 设置当前要变异的种子路径
-                resourcesManager.setCurrentMutatedSeedPath(seed.getFilepath());
-                Log.info("FuzzingManager: Current seed set to " + seed.getFilepath());
-
-                mutation.register(this.resourcesManager);
-                mutation.mutate(); // 变异
-                 if (!executor.execute(objPath)) { // TODO 可能还要别的参数
-                Log.error(executor.getResultFromConsole());
-                }
-                evaluator.eval(); // 评估
-                monitor.tearDown(); // 结束监控
+            // 获取下一个种子
+            SeedsManagerImpl.Seed seed = seedsManager.getNextSeed();
+            if (seed == null) {
+                Log.error("No seeds available to mutate.");
+                break;
             }
 
+            // 设置当前要变异的种子路径
+            resourcesManager.setCurrentMutatedSeedPath(seed.getFilepath());
+            Log.info("FuzzingManager: Current seed set to " + seed.getFilepath());
+
+            mutation.register(this.resourcesManager);
+            mutation.mutate(); // 变异
+            if (!executor.execute(objPath)) { // TODO 可能还要别的参数
+                Log.error(executor.getResultFromConsole());
+            }
+            evaluator.eval(); // 评估
+            monitor.tearDown(); // 结束监控
+        }
 
     }
 }
